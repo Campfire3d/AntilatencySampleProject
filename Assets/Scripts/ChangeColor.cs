15aff21c2409f60ffeb9561e9ffcd3aee77133da
yliu94403 <y.liu@meta-view.com>
2022-05-27 09:13:57 -0700

    able to change color if tracking task is removed

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -1,18 +1,32 @@
+using Antilatency.DeviceNetwork;
+using Antilatency.SDK;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeColor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public DeviceNetwork Network;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeTrackerColor();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ChangeTrackerColor()
     {
-        
+        var nativeNetwork = Network.NativeNetwork;
+        NodeHandle[] nodes = nativeNetwork.getNodes();
+        foreach (NodeHandle node in nodes)
+        {
+            if (nativeNetwork.nodeGetStringProperty(node, "sys/HardwareName") == "Alt")
+            {
+                Debug.Log("Trying to change Node: " + nativeNetwork.nodeGetStringProperty(node, "sys/HardwareName"));
+                nativeNetwork.nodeStartPropertyTask(node).setStringProperty("usr/LED/Color", "FFA500");
+            }
+        }
     }
 }
